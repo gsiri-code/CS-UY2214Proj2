9056c0ea5ba54d33bea2dbf6e67164c912f91da7
sirichartchai <gs3802@nyu.edu>
2024-04-22 15:14:11 -0400

    Implemented hit handling

--- simcache.cpp
+++ simcache.cpp
@@ -4,29 +4,141 @@ Adapted from Jeff Epstein
 Starter code for E20 cache Simulator
 simcache.cpp
 */
-
-#include <cstddef>
 #include <iostream>
 #include <string>
 #include <vector>
 #include <fstream>
-#include <limits>
 #include <iomanip>
 #include <regex>
-
 #include <cstdlib>
-
 
 using namespace std;
 
-struct mem_cell{
-    int tag;
-    int cycle;
+/*
+    Prints out the correctly-formatted configuration of a cache.
+
+    @param cache_name The name of the cache. "L1" or "L2"
+
+    @param size The total size of the cache, measured in memory cells.
+        Excludes metadata
+
+    @param assoc The associativity of the cache. One of [1,2,4,8,16]
+
+    @param blocksize The blocksize of the cache. One of [1,2,4,8,16,32,64])
+
+    @param num_rows The number of rows in the given cache.
+*/
+void print_cache_config(const string& cache_name, int size, int assoc, int blocksize, int num_rows) {
+    cout << "Cache " << cache_name << " has size " << size <<
+         ", associativity " << assoc << ", blocksize " << blocksize <<
+         ", rows " << num_rows << endl;
+}
+
+/*
+    Prints out a correctly-formatted log entry.
+
+    @param cache_name The name of the cache where the event
+        occurred. "L1" or "L2"
+
+    @param status The kind of cache event. "SW", "HIT", or
+        "MISS"
+
+    @param pc The program counter of the memory
+        access instruction
+
+    @param addr The memory address being accessed.
+
+    @param row The cache row or set number where the data
+        is stored.
+*/
+void print_log_entry(const string& cache_name, const string& status, int pc, int addr, int row) {
+    cout << left << setw(8) << cache_name + " " + status << right <<
+         " pc:" << setw(5) << pc <<
+         "\taddr:" << setw(5) << addr <<
+         "\trow:" << setw(4) << row << endl;
+}
+
+class Cache {
+    struct Cell {
+        int tag = 0; // holds calculated tag value
+        int cycle = -1; // -1 if not accessed yet, keeps track of cycle of access
+    };
+public:
+    Cache(const string& c_name, int c_size, int c_assoc, int c_block_size) {
+        int num_rows = size / (asso * block_size);
+
+        name = c_name;
+        size = c_size;
+        asso = c_assoc;
+        block_size = c_block_size;
+        vector<Cell> block(asso, Cell());
+        rows = vector<vector<Cell> >(num_rows, block);
+    }
+
+    Cache();
+
+    int getNumRows() { return rows.size(); }
+
+    int handleMiss(vector<Cell>& curr_block,int new_tag, int cycle) {
+        // Handle Miss
+        size_t offset = 0;
+        size_t target = 0;
+        bool found = false;
+        bool full = false;
+        while (!found) {
+            Cell curr_cell = curr_block[offset];
+            if (curr_cell.cycle > 0) {
+                // Find inValid Cell
+                found = true;
+                target = offset;
+            } else if (offset == curr_block.size() and !found){
+
+
+            }
+            offset++;
+        }
+    }
+
+    const void access(int cycle, int addr, uint16_t pc) {
+        string status = "";
+        // Get Parameters
+        int block_id = addr / block_size;
+        int row_idx = (block_id % rows.size());
+        int tag_query = block_id / rows.size();
+
+        // Index the relevant block
+        vector<Cell> curr_block = rows[row_idx];
+
+
+        for (size_t offset = 0; offset < curr_block.size(); offset++) {
+            Cell curr_cell = curr_block[offset];
+            if (curr_cell.tag == tag_query) {
+                status = "HIT"; // Update Status
+                curr_cell.cycle = cycle; // Set New cycle
+                break;
+            }
+        }
+
+        if (status != "HIT") {
+            status = "MISS"; // Update Status
+            handleMiss(curr_block,tag_query,cycle);
+
+        }
+
+
+        return print_log_entry("L1", status, pc, addr, row_idx);
+    }
+
+private:
+    string name = "";
+    int size = 0;
+    int asso = 0;
+    int block_size = 0;
+    vector<vector<Cell> > rows;
 };
 
 size_t const static NUM_REGS = 8;
 size_t const static MEM_SIZE = 1 << 13;
-
 
 void load_machine_code(ifstream& f, uint16_t mem[]) {
     regex machine_code_re("^ram\\[(\\d+)\\] = 16'b(\\d+);.*$");
@@ -53,62 +165,13 @@ void load_machine_code(ifstream& f, uint16_t mem[]) {
     }
 }
 
-/*
-    Prints out the correctly-formatted configuration of a cache.
 
-    @param cache_name The name of the cache. "L1" or "L2"
-
-    @param size The total size of the cache, measured in memory cells.
-        Excludes metadata
-
-    @param assoc The associativity of the cache. One of [1,2,4,8,16]
-
-    @param blocksize The blocksize of the cache. One of [1,2,4,8,16,32,64])
-
-    @param num_rows The number of rows in the given cache.
-*/
-void print_cache_config(const string &cache_name, int size, int assoc, int blocksize, int num_rows) {
-    cout << "Cache " << cache_name << " has size " << size <<
-        ", associativity " << assoc << ", blocksize " << blocksize <<
-        ", rows " << num_rows << endl;
-}
-
-/*
-    Prints out a correctly-formatted log entry.
-
-    @param cache_name The name of the cache where the event
-        occurred. "L1" or "L2"
-
-    @param status The kind of cache event. "SW", "HIT", or
-        "MISS"
-
-    @param pc The program counter of the memory
-        access instruction
-
-    @param addr The memory address being accessed.
-
-    @param row The cache row or set number where the data
-        is stored.
-*/
-void print_log_entry(const string &cache_name, const string &status, int pc, int addr, int row) {
-    cout << left << setw(8) << cache_name + " " + status <<  right <<
-        " pc:" << setw(5) << pc <<
-        "\taddr:" << setw(5) << addr <<
-        "\trow:" << setw(4) << row << endl;
-}
-
-
-vector<vector<mem_cell> > init_cache(int assoc, int num_rows){
-    mem_cell cell = {0,0};
-    vector<mem_cell> block(assoc, cell);
-    vector<vector<mem_cell> > new_cache (num_rows,block);
-    return new_cache;
-}
-
-void sim(uint16_t& pc, uint16_t regs[], uint16_t mem[], vector<vector<mem_cell> >& L1, vector<vector<mem_cell> > & L2, bool useL2){
+void sim(uint16_t& pc, uint16_t regs[], uint16_t mem[], Cache& L1, Cache& L2, bool useL2) {
     bool halt = false; //Set a flag for halt instruction
+    int cycle = 0;
 
     while (!halt) { //Continue to run until halt is flagged
+
         //Access Memory at current Program Counter
         uint16_t curr_ins = mem[pc & 8191]; //Read only 13 bits of pc
 
@@ -131,6 +194,7 @@ void sim(uint16_t& pc, uint16_t regs[], uint16_t mem[], vector<vector<mem_cell> 
         //Defaulted increment of Program counter
         uint16_t new_pc = pc + 1;
 
+
         if (opCode == 0b000) {
             // Three reg instructions (add, sub, or, and, slt, jr)
             if (func == 0b0000) regs[rC] = regs[rA] + regs[rB]; // add
@@ -151,11 +215,16 @@ void sim(uint16_t& pc, uint16_t regs[], uint16_t mem[], vector<vector<mem_cell> 
 
             else if (opCode == 0b010) new_pc = imm13; //j
 
-            else if (opCode == 0b100) regs[rB] = mem[(regs[rA] + imm7) & 8191];// lw
+            else if (opCode == 0b100) {
 
-            else if (opCode == 0b101) mem[(regs[rA] + imm7) & 8191] = regs[rB];// sw
+                uint16_t addr = (regs[rA] + imm7) & 8191;
+                L1.access(cycle, addr, pc);
 
-            else if (opCode == 0b110) new_pc = regs[rA] == regs[rB] ? (pc + 1 + imm7) : pc + 1;// jeq
+
+                regs[rB] = mem[(regs[rA] + imm7) & 8191];// lw
+            } else if (opCode == 0b101) {
+                mem[(regs[rA] + imm7) & 8191] = regs[rB];// sw
+            } else if (opCode == 0b110) new_pc = regs[rA] == regs[rB] ? (pc + 1 + imm7) : pc + 1;// jeq
 
             else if (opCode == 0b111) regs[rB] = regs[rA] < imm7;// slti
 
@@ -173,17 +242,16 @@ void sim(uint16_t& pc, uint16_t regs[], uint16_t mem[], vector<vector<mem_cell> 
 
         // Reset Rg0
         regs[0] = 0;
+        cycle++;
     }
 }
-
-
 
 
 /**
     Main function
     Takes command-line args as documented below
 */
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
 
     uint16_t pc = 0;
     uint16_t regArr[NUM_REGS] = {0};
@@ -192,23 +260,22 @@ int main(int argc, char *argv[]) {
     /*
         Parse the command-line arguments
     */
-    char *filename = nullptr;
+    char* filename = nullptr;
     bool do_help = false;
     bool arg_error = false;
     string cache_config;
-    for (int i=1; i<argc; i++) {
+    for (int i = 1; i < argc; i++) {
         string arg(argv[i]);
-        if (arg.rfind("-",0)==0) {
-            if (arg== "-h" || arg == "--help")
+        if (arg.rfind("-", 0) == 0) {
+            if (arg == "-h" || arg == "--help")
                 do_help = true;
-            else if (arg=="--cache") {
+            else if (arg == "--cache") {
                 i++;
-                if (i>=argc)
+                if (i >= argc)
                     arg_error = true;
                 else
                     cache_config = argv[i];
-            }
-            else
+            } else
                 arg_error = true;
         } else {
             if (filename == nullptr)
@@ -222,13 +289,13 @@ int main(int argc, char *argv[]) {
         cerr << "usage " << argv[0] << " [-h] [--cache CACHE] filename" << endl << endl;
         cerr << "Simulate E20 cache" << endl << endl;
         cerr << "positional arguments:" << endl;
-        cerr << "  filename    The file containing machine code, typically with .bin suffix" << endl<<endl;
-        cerr << "optional arguments:"<<endl;
-        cerr << "  -h, --help  show this help message and exit"<<endl;
-        cerr << "  --cache CACHE  Cache configuration: size,associativity,blocksize (for one"<<endl;
-        cerr << "                 cache) or"<<endl;
-        cerr << "                 size,associativity,blocksize,size,associativity,blocksize"<<endl;
-        cerr << "                 (for two caches)"<<endl;
+        cerr << "  filename    The file containing machine code, typically with .bin suffix" << endl << endl;
+        cerr << "optional arguments:" << endl;
+        cerr << "  -h, --help  show this help message and exit" << endl;
+        cerr << "  --cache CACHE  Cache configuration: size,associativity,blocksize (for one" << endl;
+        cerr << "                 cache) or" << endl;
+        cerr << "                 size,associativity,blocksize,size,associativity,blocksize" << endl;
+        cerr << "                 (for two caches)" << endl;
         return 1;
     }
 
@@ -239,48 +306,44 @@ int main(int argc, char *argv[]) {
     }
     load_machine_code(f, mem);
 
-
     /* parse cache config */
-    if (cache_config.size() > 0) {
+    if (cache_config.empty()) {
         vector<int> parts;
         size_t pos;
         size_t lastpos = 0;
         while ((pos = cache_config.find(",", lastpos)) != string::npos) {
-            parts.push_back(stoi(cache_config.substr(lastpos,pos)));
+            parts.push_back(stoi(cache_config.substr(lastpos, pos)));
             lastpos = pos + 1;
         }
         parts.push_back(stoi(cache_config.substr(lastpos)));
+
         int L1size = parts[0];
         int L1assoc = parts[1];
         int L1blocksize = parts[2];
-        int L1num_rows = L1size/ (L1assoc*L1blocksize);
 
-        vector<vector<mem_cell> > L1 = init_cache(L1assoc,L1num_rows);
-        print_cache_config("L1", L1size, L1assoc, L1blocksize, L1num_rows);
+        Cache L1 = Cache("L1", L1size, L1assoc, L1blocksize);
+        print_cache_config("L1", L1size, L1assoc, L1blocksize, L1.getNumRows());
 
         bool useL2 = false;
 
         if (parts.size() == 3) {
-            // TODO: execute E20 program and simulate one cache here
-            vector<vector<mem_cell> > L2 = L1;
+            Cache L2 = Cache();
+            sim(pc, regArr, mem, L1, L2, useL2);
         } else if (parts.size() == 6) {
             int L2size = parts[3];
             int L2assoc = parts[4];
             int L2blocksize = parts[5];
-            int L2num_rows = L2size/(L2assoc*L2blocksize);
             useL2 = true;
-            // TODO: execute E20 program and simulate two caches here
-            vector<vector<mem_cell> > L2 = init_cache(L2assoc,L2num_rows);
-            print_cache_config("L2", L2size, L2assoc, L2blocksize, L2num_rows);
+//             TODO: execute E20 program and simulate two caches here
+            Cache L2 = Cache("L2", L2size, L2assoc, L2blocksize);
+            print_cache_config("L2", L2size, L2assoc, L2blocksize, L2.getNumRows());
+
+            sim(pc, regArr, mem, L1, L2, useL2);
 
         } else {
-            cerr << "Invalid cache config"  << endl;
+            cerr << "Invalid cache config" << endl;
             return 1;
         }
-
-        sim(pc, regArr, mem,L1,L2,useL2);
-
     }
-
     return 0;
 }
