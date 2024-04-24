093cf8ec1c48b7097137a5302ec78fcc1d0f6d3d
sirichartchai <gs3802@nyu.edu>
2024-04-23 23:06:32 -0400

    Restructured Cache class and fixed LW SW functionality

--- simcache.cpp
+++ simcache.cpp
@@ -14,6 +14,9 @@ simcache.cpp
 
 using namespace std;
 
+size_t const static MEM_SIZE = 1 << 13;
+size_t const static NUM_REGS = 8;
+
 /*
     Prints out the correctly-formatted configuration of a cache.
 
@@ -24,7 +27,7 @@ using namespace std;
 
     @param assoc The associativity of the cache. One of [1,2,4,8,16]
 
-    @param blocksize The blocksize of the cache. One of [1,2,4,8,16,32,64])
+    @param blocksize The blocksize of the cache. One of [1,2,4,8,16,32,64]
 
     @param num_rows The number of rows in the given cache.
 */
@@ -58,110 +61,6 @@ void print_log_entry(const string& cache_name, const string& status, int pc, int
          "\trow:" << setw(4) << row << endl;
 }
 
-class Cache {
-    struct Block {
-        struct Cell {
-            int tag;
-            int cycle; // -1 if not accessed yet, keeps track of cycle @ access
-            Cell() {
-                tag = 0;
-                cycle = -1;
-            };
-        };
-
-        bool full;
-        vector<Cell> cells;
-
-        Block(int asso) {
-            cells = vector<Cell>(asso, Cell());
-            full = false;
-        }
-
-        Cell& operator[](size_t idx) { return cells[idx]; };
-
-        int size() { return cells.size(); }
-
-    };
-
-public:
-    Cache(const string& c_name, int c_size, int c_assoc, int c_block_size) {
-        name = c_name;
-
-        if (c_name != "dummy") {
-            int num_rows = c_size / (c_assoc * c_block_size);
-            print_cache_config(c_name, c_size, c_assoc, c_block_size, num_rows);
-            asso = c_assoc;
-            block_size = c_block_size;
-            rows = vector<Block>(num_rows, Block(c_assoc));
-        }
-    }
-
-    void writeCache(Block& curr_block, int new_tag, int cycle) {
-        // Handle Miss
-        size_t offset = 0;
-        size_t target = 0;
-        int min_cycle = -1;
-
-        if (curr_block.full) { // Searching for LRU in full block
-            for (size_t offset = 0; offset < curr_block.size(); offset++) {
-                if (curr_block[offset].cycle < min_cycle) { // Find least value cycle in vector
-                    min_cycle = curr_block[offset].cycle;
-                    target = offset;
-                }
-            }
-        } else
-            while (target < 1) { // Searching for empty cell in not full block
-                if (curr_block[offset].cycle > 0) {
-                    // Find inValid Cell
-                    target = offset;
-                }
-                offset++;
-            }
-
-        // Set LRU values or empty cell to MRU values
-        curr_block[target].tag = new_tag;
-        curr_block[target].cycle = cycle;
-    }
-
-    const string& getName() const { return name; }
-
-    const string& access(const string& ins, int cycle, int addr, uint16_t pc) {
-        string status = ins;
-        // Get Parameters
-        int block_id = addr / block_size;
-        int row_idx = (block_id % rows.size());
-        int tag_query = block_id / rows.size();
-
-        // Index the relevant block
-        Block curr_block = rows[row_idx];
-
-        if (ins == "LW") {
-            for (size_t offset = 0; offset < curr_block.size(); offset++) {
-                if (curr_block[offset].tag == tag_query) {
-                    status = "HIT"; // Update Status
-                    curr_block[offset].cycle = cycle; // Set New cycle
-                    break;
-                }
-            }
-            string status = "MISS";
-        }
-
-        if (status != "HIT") writeCache(curr_block, tag_query, cycle);
-
-        print_log_entry(name, status, pc, addr, row_idx);
-        return status;
-    }
-
-private:
-    string name;
-    int asso;
-    int block_size;
-    vector<Block> rows;
-};
-
-size_t const static NUM_REGS = 8;
-size_t const static MEM_SIZE = 1 << 13;
-
 void load_machine_code(ifstream& f, uint16_t mem[]) {
     regex machine_code_re("^ram\\[(\\d+)\\] = 16'b(\\d+);.*$");
     size_t expectedaddr = 0;
@@ -188,14 +87,88 @@ void load_machine_code(ifstream& f, uint16_t mem[]) {
 }
 
 
+class Cache {
+public:
+    Cache(const string& c_name, int c_size, int c_assoc, int c_block_size) {
+        name = c_name;
+
+        if (c_name != "dummy") {
+            int num_rows = c_size / (c_assoc * c_block_size);
+            print_cache_config(c_name, c_size, c_assoc, c_block_size, num_rows);
+            block_size = c_block_size;
+            vector<int> block(c_assoc, -1);
+            rows = vector<vector<int> >(num_rows, block);
+        }
+    }
+
+    void writeCache(vector<int>& curr_block, int new_tag) {
+        // shift all values down 1
+//        cout << "WRITE CACHE BEFORE:"<<endl;
+//        for (int x: curr_block){cout << x << " " ;}
+//        cout << endl;
+
+        for (size_t idx = curr_block.size() - 1; idx > 0; idx--) {
+            curr_block[idx] = curr_block[idx - 1];
+        }
+        curr_block[0] = new_tag;
+
+//        cout << "WRITE CACHE AFTER:"<<endl;
+//        for (int x: curr_block){cout << x << " " ;}
+//        cout << endl;
+    }
+
+    string handleLW(vector<int>& curr_block, int tag_query) const{
+        int target = -1;
+        for (int offset = 0; offset < curr_block.size(); offset++) {//try to find a hit
+            int current_shit = curr_block[offset];
+            if (curr_block[offset] == tag_query) {
+                target = offset;
+                break;
+            }
+        }
+        if (target > -1) { //handle hit
+            int temp = curr_block[target]; // hit value
+
+            for (int idx = target; idx > 0; idx--) {// Shift down elements to hit value in block
+                curr_block[idx] = curr_block[idx - 1];
+            }
+
+            curr_block[0] = temp; // move hit value to most recent position
+            return "HIT";
+        }
+        return "MISS";
+    }
+
+    const string& getName() const { return name; }
+
+    string access(const string& ins, int addr, uint16_t pc) {
+        string status = ins;
+        // Get Parameters
+        int block_id = addr / block_size;
+        int row_idx = (block_id % rows.size());
+        int tag_query = block_id / rows.size();
+
+        // Index the relevant block
+
+        if (ins == "LW") status = handleLW(rows[row_idx], tag_query);
+        if (status != "HIT") writeCache(rows[row_idx], tag_query);
+
+        print_log_entry(name, status, pc, addr, row_idx);
+        return status;
+    }
+
+private:
+    string name;
+    int block_size;
+    vector<vector<int> > rows;
+};
+
+
 void sim(uint16_t& pc, uint16_t regs[], uint16_t mem[], Cache& L1, Cache& L2) {
 
     bool halt = false; //Set a flag for halt instruction
-    int cycle = 0;
 
     while (!halt) { //Continue to run until halt is flagged
-//        cout << cycle << endl;
-
         //Access Memory at current Program Counter
         uint16_t curr_ins = mem[pc & 8191]; //Read only 13 bits of pc
 
@@ -241,15 +214,15 @@ void sim(uint16_t& pc, uint16_t regs[], uint16_t mem[], Cache& L1, Cache& L2) {
 
             else if (opCode == 0b100) {// lw
 
-                string L1_status = L1.access("LW", cycle, addr, pc);
+                string L1_status = L1.access("LW", addr, pc);
 
-                if (L1_status == "MISS" && L2.getName() == "L2") L2.access("LW", cycle, addr, pc);
+                if (L1_status == "MISS" && L2.getName() == "L2") L2.access("LW", addr, pc);
 
                 regs[rB] = mem[(regs[rA] + imm7) & 8191];
             } else if (opCode == 0b101) {// sw
-                string L1_status = L1.access("SW", cycle, addr, pc);
+                string L1_status = L1.access("SW", addr, pc);
 
-                if (L1_status == "SW" && L2.getName() == "L2") L2.access("SW", cycle, addr, pc);
+                if (L1_status == "SW" && L2.getName() == "L2") L2.access("SW", addr, pc);
 
                 mem[(regs[rA] + imm7) & 8191] = regs[rB];
             } else if (opCode == 0b110) new_pc = regs[rA] == regs[rB] ? (pc + 1 + imm7) : pc + 1;// jeq
@@ -270,7 +243,6 @@ void sim(uint16_t& pc, uint16_t regs[], uint16_t mem[], Cache& L1, Cache& L2) {
 
         // Reset Rg0
         regs[0] = 0;
-        cycle++;
     }
 }
 
@@ -288,85 +260,90 @@ int main(int argc, char* argv[]) {
     /*
         Parse the command-line arguments
     */
-    char* filename = nullptr;
-    bool do_help = false;
-    bool arg_error = false;
-    string cache_config;
-    for (int i = 1; i < argc; i++) {
-        string arg(argv[i]);
-        if (arg.rfind("-", 0) == 0) {
-            if (arg == "-h" || arg == "--help")
-                do_help = true;
-            else if (arg == "--cache") {
-                i++;
-                if (i >= argc)
-                    arg_error = true;
-                else
-                    cache_config = argv[i];
-            } else
-                arg_error = true;
-        } else {
-            if (filename == nullptr)
-                filename = argv[i];
-            else
-                arg_error = true;
-        }
-    }
+//    char* filename = nullptr;
+//    bool do_help = false;
+//    bool arg_error = false;
+//    string cache_config;
+//    for (int i = 1; i < argc; i++) {
+//        string arg(argv[i]);
+//        if (arg.rfind("-", 0) == 0) {
+//            if (arg == "-h" || arg == "--help")
+//                do_help = true;
+//            else if (arg == "--cache") {
+//                i++;
+//                if (i >= argc)
+//                    arg_error = true;
+//                else
+//                    cache_config = argv[i];
+//            } else
+//                arg_error = true;
+//        } else {
+//            if (filename == nullptr)
+//                filename = argv[i];
+//            else
+//                arg_error = true;
+//        }
+//    }
     /* Display error message if appropriate */
-    if (arg_error || do_help || filename == nullptr) {
-        cerr << "usage " << argv[0] << " [-h] [--cache CACHE] filename" << endl << endl;
-        cerr << "Simulate E20 cache" << endl << endl;
-        cerr << "positional arguments:" << endl;
-        cerr << "  filename    The file containing machine code, typically with .bin suffix" << endl << endl;
-        cerr << "optional arguments:" << endl;
-        cerr << "  -h, --help  show this help message and exit" << endl;
-        cerr << "  --cache CACHE  Cache configuration: size,associativity,blocksize (for one" << endl;
-        cerr << "                 cache) or" << endl;
-        cerr << "                 size,associativity,blocksize,size,associativity,blocksize" << endl;
-        cerr << "                 (for two caches)" << endl;
-        return 1;
-    }
+//    if (arg_error || do_help || filename == nullptr) {
+//        cerr << "usage " << argv[0] << " [-h] [--cache CACHE] filename" << endl << endl;
+//        cerr << "Simulate E20 cache" << endl << endl;
+//        cerr << "positional arguments:" << endl;
+//        cerr << "  filename    The file containing machine code, typically with .bin suffix" << endl << endl;
+//        cerr << "optional arguments:" << endl;
+//        cerr << "  -h, --help  show this help message and exit" << endl;
+//        cerr << "  --cache CACHE  Cache configuration: size,associativity,blocksize (for one" << endl;
+//        cerr << "                 cache) or" << endl;
+//        cerr << "                 size,associativity,blocksize,size,associativity,blocksize" << endl;
+//        cerr << "                 (for two caches)" << endl;
+//        return 1;
+//    }
 
-    ifstream f(filename);
-    if (!f.is_open()) {
-        cerr << "Can't open file " << filename << endl;
-        return 1;
-    }
+//    ifstream f(filename);
+    ifstream f("test.bin"); //Temp test shi
+//    if (!f.is_open()) {
+//        cerr << "Can't open file " << filename << endl;
+//        return 1;
+//    }
     load_machine_code(f, mem);
 
 
     /* parse cache config */
-    if (cache_config.size() > 0) {
-        vector<int> parts;
-        size_t pos;
-        size_t lastpos = 0;
-        while ((pos = cache_config.find(",", lastpos)) != string::npos) {
-            parts.push_back(stoi(cache_config.substr(lastpos, pos)));
-            lastpos = pos + 1;
-        }
-        parts.push_back(stoi(cache_config.substr(lastpos)));
+//    if (cache_config.size() > 0) {
+//        vector<int> parts;
+//        size_t pos;
+//        size_t lastpos = 0;
+//        while ((pos = cache_config.find(",", lastpos)) != string::npos) {
+//            parts.push_back(stoi(cache_config.substr(lastpos, pos)));
+//            lastpos = pos + 1;
+//        }
+//        parts.push_back(stoi(cache_config.substr(lastpos)));
 
-        // L1 parts
-        int L1size = parts[0];
-        int L1assoc = parts[1];
-        int L1blocksize = parts[2];
-        // L2 parts
-        int L2size = parts[3];
-        int L2assoc = parts[4];
-        int L2blocksize = parts[5];
+    // L1 parts
+//        int L1size = parts[0];
+//        int L1assoc = parts[1];
+//        int L1blocksize = parts[2];
 
-        Cache L1 = Cache("L1", L1size, L1assoc, L1blocksize);
-        Cache L2 = Cache("dummy", 0, 0, 0);
+    int L1size = 4;
+    int L1assoc = 1;
+    int L1blocksize = 2;
+    // L2 parts
+    int L2size = 8;
+    int L2assoc = 4;
+    int L2blocksize = 2;
 
-        if (parts.size() == 3) {
-            sim(pc, regArr, mem, L1, L2);
-        } else if (parts.size() == 6) {
-            sim(pc, regArr, mem, L1, L2);
-            Cache L2 = Cache("L2", L2size, L2assoc, L2blocksize);
-        } else {
-            cerr << "Invalid cache config" << endl;
-            return 1;
-        }
-    }
+    Cache L1 = Cache("L1", L1size, L1assoc, L1blocksize);
+//        Cache L2 = Cache("dummy", 0, 0, 0);
+
+//        if (parts.size() == 3) {
+//            sim(pc, regArr, mem, L1, L2);
+//        } else if (parts.size() == 6) {
+    Cache L2 = Cache("L2", L2size, L2assoc, L2blocksize);
+    sim(pc, regArr, mem, L1, L2);
+//        } else {
+//            cerr << "Invalid cache config" << endl;
+//            return 1;
+//        }
+//    }
     return 0;
 }
